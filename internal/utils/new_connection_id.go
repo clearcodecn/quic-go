ab77eba90cffd10758e5f5a6d01fe3867215a6e3
zhangmj <zhangmj1@dustess.com>
2023-03-26 23:51:11 +0800

    feat: init

---
+++ internal/utils/new_connection_id.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"github.com/clearcodecn/quic-go/internal/protocol"
+)
+
+// NewConnectionID is a new connection ID
+type NewConnectionID struct {
+	SequenceNumber      uint64
+	ConnectionID        protocol.ConnectionID
+	StatelessResetToken protocol.StatelessResetToken
+}
